7c7e31b32cc24dd430983d10c38e453b1f43fc1d
AlmuDiazMadramany <almumadra@hotmail.com>
2025-01-29 11:04:53 +0100

    select

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -5,6 +5,10 @@ function Filters({onChangeFilter}) {
     const handleChange=(ev) =>{
         onChangeFilter(ev.target.value);
     }
+    const handleContinentChange=()=>{
+        
+    }
+
 
   return (
     <div>
@@ -12,6 +16,23 @@ function Filters({onChangeFilter}) {
         <form>
             <div>
                 <input type="text" placeholder="Escribe el país" onChange={handleChange} />
+            </div>
+
+            <div>
+                <select onChange={handleContinentChange} >
+                <option value="">Selecciona el continente</option>
+                    <option value="All">All</option>
+                    <option value="Africa">Africa</option>
+                    <option value="North America">North America</option>
+                    <option value="South America">South America</option>
+                    <option value="Europe">Europe</option>
+                    <option value="Asia">Asia</option>
+                    <option value="Oceania">Oceania</option>
+
+
+                </select>
+
+
             </div>
         </form>
     </div>
